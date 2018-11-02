fb919f3c0d76bb5938d9c59e231333eff95a7fff
kace-study <kace.study@gmail.com>
2018-11-02 22:05:43 +0900

    step5

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -3,13 +3,19 @@ import { Card, CardImg, CardBody, CardText } from 'reactstrap';
 import './Photo.css';
 
 const Photo = ({ photo }) =>  {
+
+  onClick = (url) => {
+    window.open(url, '_blank');
+  }
+
   return (
     <Card className="mb-3 text-center">
       <CardImg 
         top 
         width="250px" 
         height="250px" 
-        src={photo.webformatURL}/>
+        src={photo.webformatURL}
+        onClick={() => onClick(photo.pageURL)} />
       <CardBody>
         <CardText>{photo.user}</CardText>
       </CardBody>
